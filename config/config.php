fd4980acfa6168988e19c65edaf7c6e7fa0bb28b
AHMAD <naufal.af0207@gmail.com>
2025-04-10 08:16:57 +0700

    update config

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-
+// cocokan dengan database mu
 $host = "localhost";
 $port = "5432";
-$dbname = "database_name"; 
-$user = "database_username";
-$password = "database_password";
+$dbname = "nama_database"; 
+$user = "username_database";
+$password = "password_database";
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
